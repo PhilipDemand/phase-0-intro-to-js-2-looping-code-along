f2adfbffd1a3e729effc507d2904dc8253df1191
Philip Demand <philipdemand@yahoo.com>
2023-01-07 23:18:30 -0500

    commit tree 412f39174dc5e5327dbc135ea9faef1a1f26bea1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+      let message = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+      newArray.push(message);
+    }
+    return newArray
+  }
+
+  function countDown(integer) {
+    while (integer >= 0) {
+      console.log(integer);
+      integer--;
+    }
+  }
